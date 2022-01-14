17c49a73cd868c4c0a99def7eb23a438a9adf540
JunkyByte <adriano.donninelli@hotmail.it>
2022-01-14 08:40:07 +0100

    added tests and new functionalties

---
+++ bookmarket/bookmarket.py
@@ -0,0 +1,125 @@
+from tinydb import TinyDB, Query, where
+from dataclasses import dataclass, field, asdict, replace
+from datetime import datetime
+from enforce_typing import enforce_types  # type: ignore
+from typing import Optional, Sequence, Union, List
+from datetime import datetime
+import time
+Q = Query()
+
+
+@enforce_types
+@dataclass(frozen=True)
+class Record:
+    url: Optional[str] = None
+    title: Optional[str] = None
+    info: Optional[str] = None
+    ts: Optional[float] = None
+
+    def query_dict(self):
+        return {x: k for x, k in asdict(self).items() if k is not None}
+
+
+# typing utility objects
+Records = Union[Record, Sequence[Record]]
+OptTimeType = Optional[Union[float, datetime]]
+
+
+class Bookmarket:
+    """
+    Main class, holds a reference to the database containing the bookmarks.
+    Manages the methods that allow to write / read / view the database.
+    Telegram etc will communicate with an instance of Bookmarket.
+    """
+    def __init__(self, db_path):
+        self.db = TinyDB(db_path)
+
+    def write(self, record: Records) -> List:
+        """
+        Add records to database.
+        Raise error if there's a duplicate
+        """
+
+        if isinstance(record, Record):
+            record = [record]
+
+        res = []
+        for r in record:
+            if self.db.get(Q.url == r.url):
+                raise FileExistsError(f'The entry {r.url!r} already exists')
+            if r.ts is None:  # We do not allow entries without timestamp
+                r = replace(r, ts=time.time())
+            elif isinstance(r.ts, datetime):
+                r = replace(r, ts=datetime.timestamp(r.ts))
+            res.append(self.db.insert(asdict(r)))
+        return res
+
+    def search(self, q) -> Optional[List[Record]]:
+        """
+        Utility function to have search queries return Record objects
+        Returns None if the query does not match anything
+        """
+        results = self.db.search(q)
+        if not results:
+            return None
+        return [Record(**r) for r in results]
+
+    def get(self, q) -> Optional[Record]:
+        """
+        Utility function to have get queries return Record objects
+        Returns None if the query does not match anything
+        """
+        result = self.db.get(q)
+        if result is None:
+            return None
+        return Record(**result)
+
+    def smatch(self, record: Record) -> Optional[List[Record]]:
+        """
+        Search for any entry that match a Record, only intialized (not None) fields are used
+        """
+        return self.search(Q.fragment(record.query_dict()))
+
+    def stime(self, start: OptTimeType = None, end: OptTimeType = None) -> Optional[List]:
+        """
+        Search in the interval of time provided.
+        Use datetime objects or timestamps,
+        if start not provided it is the epoch
+        if end not provided it is current timestamp
+        """
+        if isinstance(start, datetime):
+            start = datetime.timestamp(start)
+        elif start is None:
+            start = 0
+
+        if isinstance(end, datetime):
+            end = datetime.timestamp(end)
+        elif end is None:
+            end = time.time()
+
+        return self.search((Q.ts >= start) & (Q.ts < end))
+
+    def update(self, record: Record) -> bool:
+        """
+        Update entry that matches the record url passed.
+        Will only update record initialized fields.
+        """
+        return bool(self.db.update(record.query_dict(), Q.url == record.url))
+
+    def all(self) -> List:
+        return self.db.all()
+    
+    def truncate(self):
+        self.db.truncate()
+
+    def close(self):
+        self.db.close()
+
+
+if __name__ == '__main__':
+    db = Bookmarket('/tmp/test_db.json')
+    db.truncate()
+
+    r = Record(url='https://www.google.com', title='just google', info='A bookmark', ts=time.time())
+
+    print(r)
---
+++ test.py
@@ -0,0 +1,145 @@
+import unittest
+import time
+from dataclasses import asdict
+from datetime import datetime
+from bookmarket.bookmarket import Bookmarket, Record
+
+
+class TestDB(unittest.TestCase):
+    def setUp(self):
+        self.bm = Bookmarket('/tmp/bookmarket_db_test.py')
+
+    def tearDown(self):
+        self.bm.truncate()
+        self.bm.close()
+
+    def test_write(self):
+        r = Record(url='https://www.google.com', title='just google', info='A bookmark', ts=time.time())
+        r_id = self.bm.write(r)
+        self.assertEqual(self.bm.db.get(doc_id=r_id[0]), asdict(r))
+
+        try:  # TODO This might not be the correct place to check this works correctly.
+            r2 = Record(url='https://www.new.com', title='just google', info='A bookmark', ts=None)
+            r2_id = self.bm.write(r2)
+        except dataclasses.FrozenInstanceError:
+            self.fail('Writing a record with no timestamp raised an error')
+
+        with self.assertRaises(FileExistsError):
+            self.bm.write(r)
+    
+    def test_smatch(self):
+        t0 = time.time()
+        r_og = Record(url='https://www.google.com', title='just google', info='A bookmark', ts=t0)
+        r_og2 = Record(url='https://www.facebook.com', title='just facebook', info='happy sad', ts=time.time())
+        self.bm.write(r_og)
+        self.bm.write(r_og2)
+
+        qs = []
+        qs.append(Record(title='just google'))
+        qs.append(Record(title='just title not found'))
+        qs.append(Record(url='https://www.facebook.com'))
+        qs.append(Record(url='https://notfound.org'))
+        qs.append(Record(info='happy sad'))
+        qs.append(Record(info='not found infos'))
+        qs.append(Record(ts=t0))  # Found is r_og
+        qs.append(Record(ts=time.time()))  # Not found
+        exp_res = [r_og, None, r_og2, None, r_og2, None, r_og, None]
+
+        for i, q in enumerate(qs):
+            res = self.bm.smatch(q)
+            if exp_res[i] is not None:
+                res = res[0]
+            self.assertEqual(res, exp_res[i])
+
+    def test_stime(self):
+        # Add entries with different timestamps
+        t2012 = datetime.timestamp(datetime(2012, 1, 1))
+        r2012 = Record(url='https://www.google.com', title='just google', info='A bookmark', ts=t2012)
+
+        t2015 = datetime.timestamp(datetime(2015, 1, 1))
+        r2015 = Record(url='https://www.facebook.com', title='just facebook', info='sad', ts=t2015)
+
+        t2018 = datetime.timestamp(datetime(2018, 1, 1))
+        r2018 = Record(url='https://www.apple.com', title='just apple', info='whew', ts=t2018)
+        self.bm.write(r2012)
+        self.bm.write(r2015)
+        self.bm.write(r2018)
+
+        qs = []
+        qs.append([datetime(2010, 1, 1), datetime(2014, 1, 1)])
+        qs.append([datetime(2016, 1, 1), None])
+        qs.append([None, datetime(2016, 1, 1)])
+        qs.append([datetime(2014, 1, 1), datetime(2020, 1, 1)])
+        qs.append([datetime(2020, 1, 1), None])
+        qs.append([None, datetime(2010, 1, 1)])
+        exp_res = [[r2012], [r2018], [r2012, r2015], [r2015, r2018], None, None]
+
+        for i, (t0, t1) in enumerate(qs):
+            res = self.bm.stime(t0, t1)
+            if exp_res[i] is None:
+                self.assertEqual(res, None)
+            else:
+                self.assertEqual(set(res), set(exp_res[i]))
+
+    def test_update(self):
+        ts0 = datetime.timestamp(datetime(2010, 1, 1))
+        title0 = 'just google2'
+        info0 = 'a bookmark'
+
+        ts1 = datetime.timestamp(datetime(2020, 1, 1))
+        title1 = 'just updated title'
+        info1 = 'just updated info'
+
+        from tinydb import Query
+        Q = Query()
+
+        # Check changing different number of params with valid update Query
+        r = Record(url='https://www.google.com', title=title0, info=info0, ts=ts0)
+        self.bm.write(r)
+        r_up = Record(url='https://www.google.com', title=title1)
+        self.assertTrue(self.bm.update(r_up))
+        res = self.bm.get(Q.url == r_up.url)
+        self.assertEqual(r.ts, res.ts)
+        self.assertEqual(r.info, res.info)
+        self.assertEqual(r_up.title, res.title)
+        self.bm.truncate()
+
+        r = Record(url='https://www.google.com', title=title0, info=info0, ts=ts0)
+        self.bm.write(r)
+        r_up = Record(url='https://www.google.com', info=info1)
+        self.assertTrue(self.bm.update(r_up))
+        res = self.bm.get(Q.url == r_up.url)
+        self.assertEqual(r.ts, res.ts)
+        self.assertEqual(r.title, res.title)
+        self.assertEqual(r_up.info, res.info)
+        self.bm.truncate()
+
+        r = Record(url='https://www.google.com', title=title0, info=info0, ts=ts0)
+        self.bm.write(r)
+        r_up = Record(url='https://www.google.com', title=title1, info=info1)
+        self.assertTrue(self.bm.update(r_up))
+        res = self.bm.get(Q.url == r_up.url)
+        self.assertEqual(r.ts, res.ts)
+        self.assertEqual(r_up.title, res.title)
+        self.assertEqual(r_up.info, res.info)
+        self.bm.truncate()
+
+        r = Record(url='https://www.google.com', title=title0, info=info0, ts=ts0)
+        self.bm.write(r)
+        r_up = Record(url='https://www.google.com', title=title1, info=info1, ts=ts1)
+        self.assertTrue(self.bm.update(r_up))
+        res = self.bm.get(Q.url == r_up.url)
+        self.assertEqual(r_up.ts, res.ts)
+        self.assertEqual(r_up.title, res.title)
+        self.assertEqual(r_up.info, res.info)
+        self.bm.truncate()
+
+        # Update with non existent query (False on return of Update function)
+        r = Record(url='https://www.google.com', title=title0, info=info0, ts=ts0)
+        self.bm.write(r)
+        r_up = Record(url='https://www.notexist.com', info='fake')
+        self.assertFalse(self.bm.update(r_up))
+
+
+if __name__ == '__main__':
+    unittest.main()
